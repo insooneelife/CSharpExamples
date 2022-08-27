940501aa0e5a22e7c4c82847ecf67e6560f291f8
unknown <insooneelife@naver.com>
2022-08-27 22:19:23 +0900

    async c# server updated

---
+++ CSharpExamples/Sources/Networking/TCPSocketServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Linq;
+
+public class Program
+{
+	
+	public static int Main(String[] args)
+	{
+		AsyncTCPSocketServer.Server.StartListening();
+		return 0;
+	}
+}
